954f34d78ebc1612f17cc07bac6e11caba8f9438
John Wanjema <jonwanjema@gmail.com>
2024-02-09 13:06:23 -0800

    add function to load data from flowers folder

---
+++ fc_model.py
@@ -0,0 +1,24 @@
+import torch
+from torchvision import datasets, transforms, models
+
+
+def data_setup(data_dir,batch_size):
+    # Train trasforms
+    train_transforms = transforms.Compose([transforms.RandomRotation(10),
+                                           transforms.RandomResizedCrop(224),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor()])
+
+    # Test trasforms
+    test_transforms = transforms.Compose([transforms.Resize(255),
+                                          transforms.CenterCrop(224),
+                                          transforms.ToTensor()])
+
+    train_datasets = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
+    validation_datasets = datasets.ImageFolder(data_dir + '/valid', transform=test_transforms)
+    test_datasets = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+
+    
+    return {'train': torch.utils.data.DataLoader(train_datasets, batch_size=batch_size,shuffle=True),
+            'validation': torch.utils.data.DataLoader(validation_datasets, batch_size=batch_size),
+            'test': torch.utils.data.DataLoader(test_datasets, batch_size=batch_size)}
