e7b210129f9e223b6253a4815f21ecf26612c0a4
John Wanjema <jonwanjema@gmail.com>
2024-02-09 13:11:01 -0800

    add function to map category label to name

--- fc_model.py
+++ fc_model.py
@@ -1,6 +1,6 @@
 import torch
 from torchvision import datasets, transforms, models
-
+import json
 
 def data_setup(data_dir,batch_size):
     # Train trasforms
@@ -22,3 +22,10 @@ def data_setup(data_dir,batch_size):
     return {'train': torch.utils.data.DataLoader(train_datasets, batch_size=batch_size,shuffle=True),
             'validation': torch.utils.data.DataLoader(validation_datasets, batch_size=batch_size),
             'test': torch.utils.data.DataLoader(test_datasets, batch_size=batch_size)}
+
+def map_labels(file):
+    
+    with open(file, 'r') as f:
+        cat_to_name = json.load(f,strict=False)
+
+    return cat_to_name 
