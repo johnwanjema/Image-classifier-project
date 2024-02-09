10c2f19ec16c3f45ce80a5881c70ce5a08969faf
John Wanjema <jonwanjema@gmail.com>
2024-02-09 13:34:41 -0800

    add a function to train network and global defination for device

--- fc_model.py
+++ fc_model.py
@@ -2,8 +2,13 @@ import torch
 from torchvision import datasets, transforms, models
 import json
 from collections import OrderedDict
-from torch import nn
+from torch import nn,optim
 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
 
 def data_setup(data_dir,batch_size):
     # Train trasforms
@@ -69,3 +74,53 @@ def create_network(arch="VGG",hidden_units=512):
                           ]))
     
     return model
+
+def train(model, trainloader, testloader,epochs=1,print_every=10,learning_rate=0.001):
+
+    criterion = nn.NLLLoss()
+    optimizer = optim.AdamW(model.classifier.parameters(),learning_rate)
+    steps = 0
+    running_loss = 0
+
+    model.to(device)    
+
+    for epoch in range(epochs):
+        for inputs, labels in trainloader:
+            steps += 1
+            # Move input and label tensors to the default device
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in testloader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+
+                        test_loss += batch_loss.item()
+
+                        # Calculate accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+                print(f"Epoch {epoch+1}/{epochs}.. "
+                      f"Train loss: {running_loss/print_every:.3f}.. "
+                      f"Test loss: {test_loss/len(testloader):.3f}.. "
+                      f"Test accuracy: {accuracy/len(testloader):.3f}")
+                running_loss = 0
+                
+                model.train()
